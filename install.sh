772cafcce8f89ee05700e0b1a970204947e3bed0
ankul-shippable <ankul@shippable.com>
2016-09-22 14:26:03 +0530

    modify command #23

--- install.sh
+++ install.sh
@@ -69,4 +69,4 @@ apt-get autoremove
 echo "================= adding jfrog-cli ==================="
 wget -v https://api.bintray.com/content/jfrog/jfrog-cli-go/1.4.1/jfrog-cli-linux-amd64/jfrog?bt_package=jfrog-cli-linux-amd64 -O jfrog
 sudo chmod +x jfrog
-mv jfrog ../usr/bin/jfrog
+mv jfrog /usr/bin/jfrog
